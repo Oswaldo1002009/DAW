84949ddb9dae5e7dc98ba9c626efe56b6e76cbf8
Oswaldo1002009 <A01206725@itesm.mx>
2018-09-13 01:55:19 -0500

    Subir lab 9

---
+++ Laboratorio 9/vendor/bootstrap/js/functions.js
@@ -0,0 +1,41 @@
+function q1() {
+
+    var x = document.getElementById("ans1");
+    var y = document.getElementById("q1");
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        y.innerText = "Mostrar";
+    } else {
+        x.style.display = "block";
+        y.innerText = "Ocultar"
+    }
+}
+
+function q2() {
+
+    var x = document.getElementById("ans2");
+    var y = document.getElementById("q2");
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        y.innerText = "Mostrar";
+    } else {
+        x.style.display = "block";
+        y.innerText = "Ocultar"
+    }
+}
+
+function q3() {
+
+    var x = document.getElementById("ans3");
+    var y = document.getElementById("q3");
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        y.innerText = "Mostrar";
+    } else {
+        x.style.display = "block";
+        y.innerText = "Ocultar"
+    }
+}
